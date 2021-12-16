d3b554e5285bb605cf51f8b6373f64741ea801cc
sun053123 <phupa_sun@hotmail.co.th>
2021-12-16 21:05:43 +0700

    commit on 'main'

--- go.mod
+++ go.mod
@@ -4,16 +4,23 @@ go 1.17
 
 require (
 	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gofiber/fiber v1.14.6 // indirect
 	github.com/gofiber/fiber/v2 v2.23.0 // indirect
+	github.com/gofiber/jwt/v2 v2.2.7 // indirect
 	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/golang-jwt/jwt/v4 v4.0.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/lib/pq v1.10.4 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
--- index.go
+++ index.go
@@ -2,18 +2,158 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
+	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
+var db *sqlx.DB
+
+const jwtSecret = "SecretBOI"
+
 func main() {
+
+	var err error
+	db, err = sqlx.Open("postgres", "postgres://postgres:AsunWake053123@127.0.0.1:5432/testboi")
+
+	if err != nil {
+		panic(err)
+	}
 	app := fiber.New()
 
+	app.Use("/hello", jwtware.New(jwtware.Config{
+		SigningMethod: "HS256",
+		SigningKey:    []byte(jwtSecret),
+		SuccessHandler: func(c *fiber.Ctx) error {
+			return c.Next() // เมื่อ Token ผ่าน จะ Next ไปทำงาน Handler เลย
+		},
+		ErrorHandler: func(c *fiber.Ctx, e error) error {
+			return fiber.ErrUnauthorized
+		},
+	}))
+
+	app.Post("/signup", Signup)
+	app.Post("/login", Login)
+	app.Post("/hello", Hello)
+
+	app.Listen(":8000")
+}
+func Signup(c *fiber.Ctx) error {
+
+	request := SignupRequest{}
+	err := c.BodyParser(&request)
+
+	if err != nil {
+		return err
+	}
+
+	if request.Username == "" || request.Password == "" {
+		return fiber.ErrUnprocessableEntity
+	}
+
+	password, err := bcrypt.GenerateFromPassword([]byte(request.Password), 12)
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
+	}
+
+	query := "insert user (username, password) values (?, ?)"
+	result, err := db.Exec(query, request.Username, string(password))
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnprocessableEntity)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
+	}
+
+	user := User{
+		Id:       int(id),
+		Username: request.Username,
+		Password: string(password),
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+
+	request := LoginRequest{}
+	err := c.BodyParser(&request)
+	if err != nil {
+		return err
+	}
+
+	if request.Username == "" || request.Password == "" {
+		return fiber.ErrUnprocessableEntity
+	}
+
+	user := User{}
+	query := "select id, username, password from user where username=?"
+	err = db.Get(&user, query, request.Username)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusNotFound, "Incorrect username or password")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password)) //bc compare hash ที่ได้จาก db กับ password ที่ user ใส่เข้ามา จะไม่ไป compare ใน database
+	if err != nil {
+		return fiber.NewError(fiber.StatusNotFound, "Incorrect username or password")
+	}
+
+	claims := jwt.StandardClaims{
+		Issuer:    strconv.Itoa(user.Id),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := jwtToken.SignedString([]byte(jwtSecret))
+	if err != nil {
+		return fiber.ErrInternalServerError
+	}
+
+	return c.JSON(fiber.Map{
+		"jwtToken": token,
+	})
+
+	// return c.SendStatus(fiber.StatusOK)
+}
+
+func Hello(c *fiber.Ctx) error {
+	return nil
+}
+
+type User struct {
+	Id       int    `db:"id" json:"id"`
+	Username string `db:"username" json:"username"`
+	Password string `db:"password" json:"password"`
+}
+
+type SignupRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func Fiber() {
+	app := fiber.New(fiber.Config{
+		Prefork: true, // fork request ออกมาได้
+	})
+
 	//Middleware
-	app.Use(func(c *fiber.Ctx) error {
+	app.Use("/hello", func(c *fiber.Ctx) error {
 		c.Locals("name", "bond")
 		fmt.Println("before")
 		err := c.Next()
@@ -28,6 +168,10 @@ func main() {
 		AllowMethods: "*",
 		AllowHeaders: "*",
 	}))
+
+	// app.Use(logger.New(logger.Config{
+	// 	TimeZone: "Asia/Bangkok",
+	// }))
 
 	//GET
 	app.Get("/hello", func(c *fiber.Ctx) error {
@@ -91,6 +235,68 @@ func main() {
 	//New Error
 	app.Get("/error", func(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusNotFound, "cannot found")
+	})
+
+	//Group ใช้ทำ version
+	v1 := app.Group("/v1", func(c *fiber.Ctx) error {
+		c.Set("Version", "v1") //set header version ก่อน ค่อยเรียก next
+		return c.Next()
+	})
+	v1.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello v1")
+	})
+
+	v2 := app.Group("/v2", func(c *fiber.Ctx) error {
+		c.Set("Version", "v2")
+		return c.Next()
+	})
+
+	v2.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello v2")
+	})
+
+	//Mount
+	userApp := fiber.New()
+	userApp.Get("/login", func(c *fiber.Ctx) error {
+		return c.SendString("Login")
+	})
+
+	app.Mount("/user", userApp) //นำไปใช้กับ app , userapp จะ contrl ทั้ง path /user
+
+	//Server
+	app.Server().MaxConnsPerIP = 1 // example set connection ต่อ 1 ip = 1 ครั้ง
+	app.Get("/server", func(c *fiber.Ctx) error {
+		time.Sleep(time.Second * 30)
+
+		return c.SendString("server")
+	})
+
+	//Get ENV
+	app.Get("/env", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"BaseURL":     c.BaseURL(),
+			"Hostname":    c.Hostname(),
+			"IP":          c.IP(),
+			"IPs":         c.IPs(),
+			"OriginalURL": c.OriginalURL(),
+			"Path":        c.Path(),
+			"Protocol":    c.Protocol(),
+			"Subdomains":  c.Subdomains(),
+		})
+	})
+
+	//Body
+	app.Post("/body", func(c *fiber.Ctx) error {
+		fmt.Printf("IsJson: %v", c.Is("json"))
+		fmt.Println(string(c.Body()))
+
+		person := Person{}
+		err := c.BodyParser(&person)
+		if err != nil {
+			return err
+		}
+		fmt.Println(person)
+		return nil
 	})
 
 	app.Listen(":8000")
